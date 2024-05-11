3af299ea141f2524ac506f8440baea19bb6df5fa
郑佳康 <10678805+zheng-jk@user.noreply.gitee.com>
2024-05-11 09:47:35 +0800

    gsap 调整为本地js文件、结合jq

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-const main = document.querySelector('#main')
-const f_box = document.querySelector('.first_box');
-const s_box = document.querySelector('.second_box');
-const t_box = document.querySelector('.third_box');
 // document.addEventListener('visibilitychange', () => {
 //     console.log('可见度变化了');
 // })
@@ -16,32 +12,31 @@ const throttle = (func, time) => {
     }, time);
   }
 }
-// 获取元素样式
+// 获取元素样式 原生方法
 const getPlace = (el, k) => {
   return el.getBoundingClientRect()[k]
 }
 // 判断是否出现在可视区
 const isVisible = el => {
   // let width = getPlace(el, 'width');
-  let height = getPlace(el, 'width');
-  return getPlace(el, 'top') < (window.innerHeight - height / 2) && getPlace(el, 'top') > - height / 2 && getPlace(el, 'left') < window.innerWidth && getPlace(el, 'left') > 0;
+  let gapTop = el.offset().top - $(window).scrollTop();
+  let gapLeft = el.offset().left - $(window).scrollLeft();
+  let height = el.height();
+  return gapTop < (window.innerHeight - height / 2) && gapTop > - height / 2 && gapLeft < window.innerWidth && gapLeft > 0;
 }
+let arr = [
+  { el: $('.first_box'), cl: '.first_box', x: 360 },
+  { el: $('.second_box'), cl: '.second_box', x: 660 },
+  { el: $('.third_box'), cl: '.third_box', x: 880 },
+]
 const handleScroll = () => {
-  if (isVisible(f_box)) {
-    gsap.to('.first_box', { x: 360 });
-  } else {
-    gsap.to('.first_box', { x: 0 });
-  }
-  if (isVisible(s_box)) {
-    gsap.to('.second_box', { x: 660 });
-  } else {
-    gsap.to('.second_box', { x: 0 });
-  }
-  if (isVisible(t_box)) {
-    gsap.to('.third_box', { x: 660 });
-  } else {
-    gsap.to('.third_box', { x: 0 });
-  }
+  arr.forEach(val => {
+    if(isVisible(val.el)) {
+      gsap.to(val.cl, { x: val.x })
+    } else {
+      gsap.to(val.el, { x: 0 })
+    }
+  })
 }
 handleScroll();
-main.addEventListener('scroll', throttle(handleScroll, 200));
+$('#main').on('scroll', throttle(handleScroll, 200))
